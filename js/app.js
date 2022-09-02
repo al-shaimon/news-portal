b3a98c30a94f15c802fa4e716f857b1ab24a3c97
al-shaimon <alshaimon968@gmail.com>
2022-09-03 02:33:00 +0600

    news categories added and figure out api is an object

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
-console.log('app connected');
+// categories
+const loadCategories = async () => {
+  const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+  const data = await response.json();
+  return data;
+};
+
+const setCategories = async () => {
+  const data = await loadCategories();
+  // console.log(data);
+  for (const key in data.data) {
+    console.log(data);
+  }
+};
+
+setCategories();
+// loadCategories();
