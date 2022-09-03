62dc67608f2a1330375cf905b1c37329a2a9bba1
al-shaimon <alshaimon968@gmail.com>
2022-09-03 23:03:44 +0600

    html redesign using tailwind

--- js/app.js
+++ js/app.js
@@ -8,8 +8,23 @@ const loadCategories = async () => {
 const setCategories = async () => {
   const data = await loadCategories();
   // console.log(data);
+
+  const menu = document.getElementById('news-categories');
+
   for (const key in data.data) {
     console.log(data);
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="col">${data.data.news_category[0].category_name}</div>
+    <div class="col">${data.data.news_category[1].category_name}</div>
+    <div class="col">${data.data.news_category[2].category_name}</div>
+    <div class="col">${data.data.news_category[3].category_name}</div>
+    <div class="col">${data.data.news_category[4].category_name}</div>
+    <div class="col">${data.data.news_category[5].category_name}</div>
+    <div class="col">${data.data.news_category[6].category_name}</div>
+    <div class="col">${data.data.news_category[7].category_name}</div>
+    `;
+    menu.appendChild(div);
   }
 };
 
